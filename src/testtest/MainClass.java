8f4feda2631b04025770ee72c6961b8124101923
moonsungmin <mkmm1104@naver.com>
2023-06-23 14:03:42 +0900

    testest

---
+++ src/testtest/MainClass.java
@@ -0,0 +1,26 @@
+package testtest;
+
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("최초 금액을 입력해주세요. : ");
+		int firMo = sc.nextInt();
+		
+		while(true) {
+			System.out.println("1. 목록 확인 및 장바구니 담기");
+			System.out.println("2. 장바구니 확인 및 삭제");
+			System.out.println("3. 결제 및 잔금 확인");
+			System.out.println("4. 영수증 출력");
+			System.out.print(">>> : ");
+			int num = sc.nextInt();
+			switch(num) {
+				case 1: break;
+				case 2: break;
+				case 3: break;
+				case 4: break;
+			}
+		}
+	}
+}
