8037ee720be3c675c05b0e0a9fae6408fd3ab4f6
moonsungmin <mkmm1104@naver.com>
2023-06-23 14:41:27 +0900

    testest

--- src/testtest/list_del.java
+++ src/testtest/list_del.java
@@ -10,38 +10,42 @@ public class list_del {
 		
 		TestDTO dto = new TestDTO();
 		
+		while(true) {
 		System.out.println("장바구니 목록");
 		System.out.println("1. 휴지  : " + dto.getA());
 		System.out.println("2.  물	: " + dto.getB());
 		System.out.println("3. 라면 	: " + dto.getC());
 		System.out.println("4. 세제 	: " + dto.getD());
 		System.out.println("5. 수건 	: " + dto.getE());
+		System.out.println("6. 나가기");
 		
 		System.out.print("삭제할 목록 >>> ");
 		int num = sc.nextInt();
-		
-		switch(num) {
-		case 1:
-			int mod = mod_list(dto.getA());
-			dto.setA(mod);
-			break;
-		case 2:
-			mod = mod_list(dto.getB());
-			dto.setB(mod);
-			break;
-		case 3:
-			mod = mod_list(dto.getC());
-			dto.setC(mod);
-			break;
-		case 4:
-			mod = mod_list(dto.getD());
-			dto.setD(mod);
-			break;
-		case 5:
-			mod = mod_list(dto.getE());
-			dto.setE(mod);
-			break;
-		}
+			switch(num) {
+				case 1:
+					int mod = mod_list(dto.getA());
+					dto.setA(mod);
+					break;
+				case 2:
+					mod = mod_list(dto.getB());
+					dto.setB(mod);
+					break;
+				case 3:
+					mod = mod_list(dto.getC());
+					dto.setC(mod);
+					break;
+				case 4:
+					mod = mod_list(dto.getD());
+					dto.setD(mod);
+					break;
+				case 5:
+					mod = mod_list(dto.getE());
+					dto.setE(mod);
+					break;
+				case 6:
+					return ;
+			}
+		}	
 	}
 	
 	public int mod_list(int dto_num) {
