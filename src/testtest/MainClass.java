91232bf2e2767409a5d1563271a8278095be2ee4
ye yu jin <hina0510@naver.com>
2023-06-23 14:41:32 +0900

    3 추가

--- src/testtest/MainClass.java
+++ src/testtest/MainClass.java
@@ -2,11 +2,15 @@ package testtest;
 
 import java.util.Scanner;
 
+import third.ThirdNum;
+
 public class MainClass {
 	public static void main(String[] args) {
+		ThirdNum thi = new ThirdNum();
 		Scanner sc = new Scanner(System.in);
 		System.out.print("최초 금액을 입력해주세요. : ");
 		int firMo = sc.nextInt();
+		int mon;
 		
 		while(true) {
 			System.out.println("1. 목록 확인 및 장바구니 담기");
@@ -18,7 +22,11 @@ public class MainClass {
 			switch(num) {
 				case 1: break;
 				case 2: break;
-				case 3: break;
+				case 3: 
+					thi.payMon();
+					mon = firMo - thi.sum;
+					System.out.println("남은 금액 : "+mon);
+					break;
 				case 4: break;
 			}
 		}
